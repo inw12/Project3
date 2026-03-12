60002ef4de40a66cd9c5d244cc98204177b5f39a
iann <ianathanwong@gmail.com>
2026-03-11 21:28:29 -0700

    Dodge Action

--- Project3/Assets/Scripts/PlayerMovement.cs
+++ Project3/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 public struct MovementInput
 {
     public Vector2 Movement;
@@ -14,15 +15,27 @@ public enum CurrentAction
 {
     Idle, Move, Dodge
 }
+
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    private struct DodgeInfo
+    {
+        public Vector3 Direction;
+        public bool Triggered;
+        public float Timer;
+    }
+
+    [Space]
     [SerializeField] private float moveSpeed = 10f;
     [Space]
+    [SerializeField] private CapsuleCollider hurtbox;
     [SerializeField] private float dodgeSpeed= 7f;
     [SerializeField] private float dodgeDuration = 1f;
+    private DodgeInfo _dodgeInfo;
 
     private CharacterController _controller;
+    private bool _inputEnabled;
 
     // Requested Inputs
     private Vector3 _requestedMovement;
@@ -36,6 +49,9 @@ public class PlayerMovement : MonoBehaviour
     {
         // CharacterController
         _controller = GetComponent<CharacterController>();
+
+        // Player Input
+        _inputEnabled = true;
         
         // State Machine
         _state.Action = CurrentAction.Idle;
@@ -47,11 +63,28 @@ public class PlayerMovement : MonoBehaviour
     // Should be called in UPDATE() in 'Player'
     public void UpdateInput(MovementInput input)
     {
-        // Movement Direction
-        _requestedMovement = new Vector3(input.Movement.x, 0f, input.Movement.y).normalized;
+        if (_inputEnabled)
+        {
+            // Movement Direction
+            _requestedMovement = new Vector3(input.Movement.x, 0f, input.Movement.y).normalized;
 
-        // Dodge Input
-        _requestedDodge = input.Dodge;
+            // Dodge Input
+            _requestedDodge = input.Dodge;
+            // Trigger Dodge (if pressed)
+            if (_requestedDodge && !_dodgeInfo.Triggered)
+            {
+                // Update dodge info
+                _dodgeInfo.Triggered = true;
+                _dodgeInfo.Direction = _requestedMovement;
+                _dodgeInfo.Timer = 0f;
+
+                // Disable hurtbox
+                hurtbox.enabled = false;
+
+                // Disable player inputs
+                _inputEnabled = false;
+            }
+        }
     }
 
     // Should be called in FIXEDUPDATE() in 'Player'
@@ -66,14 +99,38 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        // Apply Movement
-        var targetVelocity = deltaTime * moveSpeed * _requestedMovement;
-        _controller.Move(targetVelocity);
+        // Dodge Movement
+        if (_dodgeInfo.Triggered)
+        {
+            _state.Action = CurrentAction.Dodge;
+            _dodgeInfo.Timer += deltaTime;
 
-        // Update State Machines
+            // Sustain this movement during dodge duration
+            var targetVelocity = deltaTime * dodgeSpeed * _dodgeInfo.Direction;
+            _controller.Move(targetVelocity);
+
+            // Reset everything once dodge duration reached
+            if (_dodgeInfo.Timer > dodgeDuration)
+            {
+                _dodgeInfo.Triggered = false;
+                _inputEnabled = true;
+                hurtbox.enabled = true;
+            }
+        }
+        // Regular Movement
+        else if (_requestedMovement.sqrMagnitude > 0f)
+        {
+            _state.Action = CurrentAction.Move;
+            var targetVelocity = deltaTime * moveSpeed * _requestedMovement;
+            _controller.Move(targetVelocity);
+        }
+        else
+        {
+            _state.Action = CurrentAction.Idle;
+        }
+
+        // Update State Machine
         _state.Velocity = _controller.velocity;
-
-        // Update Previous State
         _prevState = _state;
     }
 }
