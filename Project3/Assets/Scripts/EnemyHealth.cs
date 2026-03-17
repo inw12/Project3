f6a74c1aa6a61d9657636b8b06bacf5b992fe259
iann <ianathanwong@gmail.com>
2026-03-17 09:53:41 -0700

    Enemy Health Script

---
+++ Project3/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float health;
+    private float _currentHealth;
+    private bool _isAlive;
+
+    void Start()
+    {
+        _currentHealth = health;
+        _isAlive = _currentHealth > 0f;
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        _currentHealth += amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0f, health);
+    }
+    public void DecreaseHealth(float amount)
+    {
+        _currentHealth = amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0f, health);
+        _isAlive = _currentHealth > 0f;
+    }
+}
