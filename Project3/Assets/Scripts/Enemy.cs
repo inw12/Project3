29b684c7f7ee28a7bfa52715f78fcf961c2e220c
iann <ianathanwong@gmail.com>
2026-03-19 16:32:25 -0700

    Enemy Script Setup

--- Project3/Assets/Scripts/Enemy.cs
+++ Project3/Assets/Scripts/Enemy.cs
@@ -1,15 +1,71 @@
+/// 
+/// * This script is responsible for...
+///     - State machine control
+///     - Determining what actions to do depending on the state
+/// 
 using UnityEngine;
+
+public struct EnemyState
+{
+    public EnemyAction CurrentAction;
+    public EnemyAttack CurrentAttack;
+}
+public enum EnemyAction
+{
+    Idle,
+    Move,
+    Attack,
+    Stagger
+}
+public enum EnemyAttack
+{
+    None,
+    Ranged,
+    FocusedRanged,
+    Melee,
+    Zone
+}
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Enemy Components")]
+    [SerializeField] private EnemyMovement enemyMovement;
+    [Header("Basic Stats")]
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float moveSpeed = 15f;
+
+    // "Who are we fighting?"
+    private Transform _target;    
+
+    // State Machine Control
+    private EnemyState _state;
+    private EnemyState _prevState;
+
     void Start()
+    {
+        // Set target to be the player
+        if (Player.Instance) _target = Player.Instance.transform;
+
+        // Initialize Enemy Components
+        enemyMovement.Initialize(moveSpeed);
+
+        // Initialize State Machine
+        _state.CurrentAction = EnemyAction.Idle;
+        _state.CurrentAttack = EnemyAttack.None;
+        _prevState = _state;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
+    {
+        _prevState = _state;
+    }
+
+    void FixedUpdate()
     {
         
     }
