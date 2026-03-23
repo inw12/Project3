a3b73ee3a1d06a397eb469156c14a5b5458686ba
iann <ianathanwong@gmail.com>
2026-03-23 12:58:20 -0700

    BurstShot Animation Imported

---
+++ Project3/Assets/Scripts/2_Enemy/EnemyAttacks/RangedAttacks/BurstShot.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class BurstShot : EnemyRangedAttack
+{
+    public override void Attack(Transform target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
